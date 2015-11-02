68b3330cbc2685cb424d44a5f980e4c80c1d9758
Carlos Melo <carlos.melo_desenv@hotmail.com>
2015-11-02 10:03:02 -0200

    Criando o projeto de Teste

---
+++ RegrasScriptSQLTest/ConteudoDMLUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RegrasScriptSQLTest
+{
+    [TestClass]
+    public class ConteudoDMLUnitTest
+    {
+        [TestMethod]
+        public void ConteudoDML()
+        {
+        }
+    }
+}
---
+++ RegrasScriptSQLTest/ConteudoTextoReservadoUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RegrasScriptSQLTest
+{
+    [TestClass]
+    public class ConteudoTextoReservadoUnitTest
+    {
+        [TestMethod]
+        public void ConteudoTextoReservado()
+        {
+        }
+    }
+}
